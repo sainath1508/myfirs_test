2d39fcde924e23f736ff429373fcaadb41e292cf
sai <smanapati1997@gmail.com>
2025-10-28 15:59:45 +0530

    for testing war

---
+++ src/test/java/myfirst_project/demo/TripApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst_project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
